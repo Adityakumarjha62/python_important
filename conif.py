f190e2c3fbe4308dd830be6b6e4658b747f5f110
Aditya Kumar Jha <jadityakumar5@gmail.com>
2026-09-23 00:07:01 +0530

    conif.py

---
+++ conif.py
@@ -0,0 +1,54 @@
+# find the Number positive, negative abd  zero to check .
+a=4
+if a > 0:
+    print("positive")
+elif a < 0:
+    print("negative")    
+else:
+    print("zero")    
+# Find to check  Number even and  odd .
+a=18
+if a % 2 ==0:
+    print("even")
+else:
+    print("odd")    
+# Find the largest of two numbers .
+a=90
+b=23
+if a > b and b < a:
+    print("largest is a =",a)
+else:
+    print("largest is b =",b)    
+# Find the lagrest of three number.
+a=214
+b=44
+c=41
+if a > b and a > c:
+    print("The largest number is a =",a)
+elif b > c and b > a:
+    print("The largest number is b =",b)   
+else:
+    print("The largest number is c =",c)     
+# To check Student pass/fail .
+num=30
+if num >= 30:
+    print("pass")
+else:
+    print("fail")    
+# print grade accourding to marks .
+marks=28
+if marks >= 90:
+    print("grade A")
+elif marks >= 60:
+    print("grade B")  
+elif marks >=30:
+    print("grade C")    
+else:
+    print("grade D fail")   
+
+#  number 5 to check divisible or not divisible.
+num=30
+if num %5 ==0:
+    print("divisible")
+else:
+    print("not divisible")    
